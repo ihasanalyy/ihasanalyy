7402c2de94e50b5f663b2bbe44dc6779ca2d3e53
Hassan Ali <ihasanalyy@gmail.com>
2024-04-28 18:38:10 +0500

    practice chap to 55

---
+++ Module 2 JS/Practice to Chap 55/app.js
@@ -0,0 +1,47 @@
+// function ChangeImg (event,element){
+// // console.log("ChangeImg",event,element)
+// if(event == 'over' ){
+//     element.src = "./Image/car2.jpg"
+// }else{
+//     element.src = "./Image/car1.jpg"
+// }
+// }
+
+
+
+
+// function ChangeImg(event,element){
+// // console.log('ChangeImg',event,element)
+// if(event == 'over'){
+//     element.src="./Image/bulb.on.png"
+//     }else{
+//         element.src="./Image/bulb.off.png"
+//     }}
+
+
+
+// function inputFocus(element){
+// element.style.backgroundColor = 'grey'
+// }
+
+
+
+
+
+// function inputBlur(element){
+// // console.log('inputFocus',element.value.length)
+// if(element.value.length < 3 ){
+//     element.style.backgroundColor = 'red'
+//    }else{
+//        element.style.backgroundColor = 'blue'
+//    }
+//    } 
+
+
+
+
+
+
+
+
+
